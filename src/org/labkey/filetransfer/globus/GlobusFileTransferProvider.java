5219e03fcaf4c96962514d811fc5fbd67650a869
Adam Rauch <adam@labkey.com>
2022-09-21 14:59:21 -0700

    Repackage JSONObject and JSONArray (#34)

--- src/org/labkey/filetransfer/globus/GlobusFileTransferProvider.java
+++ src/org/labkey/filetransfer/globus/GlobusFileTransferProvider.java
@@ -32,7 +32,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.json.JSONObject;
+import org.json.old.JSONObject;
 import org.labkey.api.data.Container;
 import org.labkey.api.security.User;
 import org.labkey.api.util.PageFlowUtil;
